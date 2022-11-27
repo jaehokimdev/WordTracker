15796f435a118e24e5ca2e27fe74ce99da0ca430
Jaeho Kim <101899896+jaehokimdev@users.noreply.github.com>
2022-11-27 00:28:22 -0700

    doing populate file

--- src/manager/WordTracker.java
+++ src/manager/WordTracker.java
@@ -1,22 +1,95 @@
 package manager;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.StringTokenizer;
+
+import wordDomain.Word;
+
 public class WordTracker {
 	private String inputFileName;
 	private String option;
 	private String outputFileName;
+	private static int N;
 	
 	public WordTracker(String inputFile, String options) {
 		this.inputFileName = inputFile;
 		this.option = options;
+		
+		populateFromTextFile(inputFile);
 	}
 	
 	public WordTracker(String inputFile, String options, String outputFile) {
 		this.inputFileName = inputFile;
 		this.option = options;
 		this.outputFileName = outputFile;
+		populateFromTextFile(inputFile);
 	}
 	
-	public void readWordFromText() {
-		
+	public static void deserializeWordFromFile()
+	{
+		try
+		{
+			ObjectInputStream ois = new ObjectInputStream(
+						new FileInputStream("res/repository.ser"));
+			
+			for(int i = 0; i < N; i++)
+			{
+				Word word = (Word)ois.readObject();
+				System.out.println(word);
+			}
+			ois.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 	}
+	
+	public void populateFromTextFile(String inputFile) {
+		try {
+			BufferedReader file = new BufferedReader(new FileReader(inputFile));
+		
+			String line = file.readLine();
+			
+			StringTokenizer st = new StringTokenizer(line);
+									
+			while (st.hasMoreTokens()) {
+				System.out.println(st.nextToken());
+			}
+			
+	
+			file.close();
+		
+		}catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (SecurityException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
--- src/wordDomain/Word.java
+++ src/wordDomain/Word.java
@@ -6,5 +6,32 @@ public class Word {
 	private String word;
 	private LinkedList<Location> locations;
 	
+	public Word(String word, LinkedList<Location> locations) {
+		this.word = word;
+		this.locations = locations;
+	}
 	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public LinkedList<Location> getLocations() {
+		return locations;
+	}
+	public void setLocations(LinkedList<Location> locations) {
+		this.locations = locations;
+	}
+	
+	public int compareTo(Word otherWord) {
+		
+		if (this.word.compareToIgnoreCase(otherWord.getWord()) > 0) {
+			return 1;
+		} else if (this.word.equalsIgnoreCase(otherWord.getWord())) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
 }
